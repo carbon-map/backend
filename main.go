90c279d022044d931beb18e07ef9cf0dc2d36868
Jason0411202 <Jason0411202@gmail.com>
2023-12-20 18:57:27 +0800

    add CORS

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"backend/model"   //引入model套件
 	"backend/routing" //引入routing套件
 
+	"github.com/labstack/echo/v4/middleware"
+
 	"github.com/labstack/echo/v4" //引入echo套件
 )
 
@@ -11,6 +13,11 @@ func main() {
 	log := model.InitLogger()
 	e := echo.New()               //建立一個Echo的物件
 	e.Static("/static", "static") //註冊靜態檔案路徑
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},                                        // 允許所有來源
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE}, // 允許的 HTTP 方法
+	}))
 
 	routing.Routing(e) //將建立的Echo物件傳入routing() function
 
